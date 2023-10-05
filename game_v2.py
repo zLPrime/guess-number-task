cae5d36c72ce31e0a492c817a4233b1a3346c894
Jakub Charabet <jkharabet@gmail.com>
2023-10-05 18:40:34 +0200

    initial commit

---
+++ game_v2.py
@@ -0,0 +1,76 @@
+"""Игра угадай число
+Компьютер сам загадывает и сам угадывает число
+"""
+
+import numpy as np
+
+
+def random_predict(number: int = 1) -> int:
+    """Рандомно угадываем число
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0
+
+    while True:
+        count += 1
+        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if number == predict_number:
+            break  # выход из цикла если угадали
+    return count
+
+def binary_predict(number: int = 1) -> int:
+    """Угадываем число методом бинарного поиска
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+
+    count = 0
+
+    predict_number = 50
+
+    step = 25
+
+    while True:
+        count += 1
+        if number < predict_number:
+            predict_number -= step
+        else:
+            predict_number += step
+        if number == predict_number:
+            break
+        step = max(1, int(step/2))
+    return count
+
+def score_game(predict_fn) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(predict_fn(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+
+if __name__ == "__main__":
+    # RUN
+    score_game(binary_predict)
